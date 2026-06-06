0c5def7eb0ba8accb4ccdd36908c3d0346060d59
Aman Coder <amancoder28@gmail.com>
2026-06-06 22:08:01 +0300

    No, Streaming SSR, I Guess

--- src/routes/_authed.tsx
+++ src/routes/_authed.tsx
@@ -2,45 +2,20 @@
 import { ServerSessionQueryOptions } from "@db/query.functions";
 import { useQuery } from "@tanstack/vue-query";
 import { createFileRoute, Outlet } from "@tanstack/vue-router";
-import { defineComponent, Suspense } from "vue";
-
-const RouteComponent = defineComponent({
-  setup() {
-    return () => (
-      <>
-        <Suspense>
-          {{
-            default: () => <Home />,
-            fallback: () => "Loading Middleman...",
-          }}
-        </Suspense>
-      </>
-    );
-  },
-});
-
-const Home = defineComponent({
-  async setup() {
-    const query = useQuery(() => ServerSessionQueryOptions());
-
-    await query.suspense();
-
-    return () => {
-      const data = query.data.value;
-
-      return (
-        <main class="mb-6" id="content">
-          {data?.user?.name ? <Outlet /> : <h1>Not Logged In</h1>}
-        </main>
-      );
-    };
-  },
-});
 
 export const Route = createFileRoute("/_authed")({
   component: RouteComponent,
   beforeLoad: async ({ context }) => {
-    // oxlint-disable-next-line typescript/no-floating-promises
-    context.queryClient.prefetchQuery(ServerSessionQueryOptions());
+    await context.queryClient.prefetchQuery(ServerSessionQueryOptions());
   },
 });
+
+function RouteComponent() {
+  const query = useQuery(() => ServerSessionQueryOptions());
+
+  return (
+    <main class="mb-6" id="content">
+      {query.data.value?.user?.name ? <Outlet /> : <h1>Not Logged In</h1>}
+    </main>
+  );
+}
